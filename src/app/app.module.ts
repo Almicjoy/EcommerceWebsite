b627e6e64d0e0eb5a71d92ee114177175f80c0c1
Alessa Castillo <amcastillo@valdosta.edu>
2023-02-02 15:17:50 -0600

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
